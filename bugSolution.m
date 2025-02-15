b1ca7694105be5cdc0e4fba860ea7c81f0a45592
BugHunter-X <askzenai@gmail.com>
2025-02-15 23:34:06 +0100

    Add bugSolution.m file

---
+++ bugSolution.m
@@ -0,0 +1,15 @@
+function result = myFunction(input)
+  % Some code here
+  input = double(input); % Explicitly cast input to double precision
+  if input > 10
+    result = input * 2; 
+  else
+    result = input / 2; 
+  end
+end
+
+% Example usage
+input = 5;
+output = myFunction(input);  % output will be 2.5
+input = 15;
+output = myFunction(input); % output will be 30
